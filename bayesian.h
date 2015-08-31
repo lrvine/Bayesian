c569e8b08440277282fce14822f8d26773708320
tsungyeh <tsungyeh@gmail.com>
2015-09-01 02:58:40 +0800

    fix compile error of base clas bayesian

--- bayesian.h
+++ bayesian.h
@@ -1,12 +1,13 @@
+#ifndef bayesian_h
+#define bayesian_h
 class bayesian
 {
 protected:
 	int traininstances;  //store the number of training instances
 	int testinstances;   //store the number of testing instances
 	int attributes;      //store the number of attributes
-	virtual	void classifier(long double ***  , int * , double * , int **, char*);
 	//calculate the probability of each choice and choose the greatest one as our prediction
 	void accuracy(int * , int * ); // claculate the accuracy
 
-
 };
+#endif
--- naivebayesian.cpp
+++ naivebayesian.cpp
@@ -269,26 +269,3 @@ void naivebayesian::classifier(long double** protable,int*numclass ,double* coun
 	delete [] temp;
 	delete [] outcome;
 }
-
-
-//calculate the accuracy
-void naivebayesian::accuracy(int *outcome , int * result)
-{
-
-
-	double correct=0;// store the number of correct predictions
-
-	for( int i=0 ; i<testinstances; i++)//count the number of correct predictions 
-	{
-		if (outcome[i]==result[i])
-			correct++;
-
-		cout<<"predict to be "<<outcome[i]<<" is actually "<<result[i]<<endl;
-	}
-	
-	cout<<"total "<<testinstances<<" data hve "<<correct<<" correct prediction"<< endl;
-
-	double percentage=correct/testinstances; // calculate the accuracy
-
-	cout<<"accuracy is "<<percentage*100<<"%"<<endl;
-}
