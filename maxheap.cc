53f92449683865287146c1903b8b0a42a026b08f
lrvine <tsungyeh@gmail.com>
2016-01-29 00:47:42 +0800

    fix formatting

--- maxheap.cc
+++ maxheap.cc
@@ -19,18 +19,6 @@ maxheap<Type>::maxheap(int sz )
 	heap=new data<Type>[maxsize+1];
 }
 
-/*
-template<class Type>
-void maxheap<Type>::printcontent(int number)
-{
-	cout<<"key value" <<setw(12)<<"content"<<endl;
-    for(int i=1; i<=number; i++)
-	{
-	cout<<setw(6)<<heap[i].key<<setw(15)<<heap[i].value<<endl;
-	}
-}
-*/
-
 template<class Type>
 void maxheap<Type>::insert(const data<Type> x)
 {
@@ -61,32 +49,31 @@ void maxheap<Type>::deletemax(data<Type>& x)
 		i=j;j*=2;
 	}
 	heap[i]=k;
-//	return &x;
 }
 
 template<class Type>
 bool maxheap<Type>::isfull()
 {
-  if (n==maxsize) return true;
-  else return false;
+	if (n==maxsize) return true;
+	else return false;
 }
 
 template<class Type>
 bool maxheap<Type>::isempty()
 {
-  if (n==0) return true;
-  else return false;
+	if (n==0) return true;
+	else return false;
 }
 
 template<class Type>
 void maxheap<Type>::heapfull()
 {
-  cerr<<"heap is already full !!"<<endl; exit(1);
+	cerr<<"heap is already full !!"<<endl; exit(1);
 }
 
 template<class Type>
 void maxheap<Type>::heapempty()
 {
-  cerr<<"heap is empty !!"<<endl;exit(1);
+	cerr<<"heap is empty !!"<<endl;exit(1);
 }
 
